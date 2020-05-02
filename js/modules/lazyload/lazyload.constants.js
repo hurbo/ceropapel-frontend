64edb5d5f57e27b7107f5009c661bbd4e643b0e3
jdorame <jdorame@metecsis.mx>
2020-05-02 06:07:46 -0600

    chore(*): actualizar lazyload y limpiar dependencias

--- js/modules/lazyload/lazyload.constants.js
+++ js/modules/lazyload/lazyload.constants.js
@@ -7,25 +7,11 @@
       // jQuery based and standalone scripts
       scripts: {
         'moment': ['vendor/moment/min/moment-with-locales.min.js'],
-        'whirl': ['vendor/whirl/dist/whirl.css'],
-        'fastclick': ['vendor/fastclick/lib/fastclick.js'],
         'modernizr': ['vendor/modernizr/modernizr.custom.js'],
         'animate': ['vendor/animate.css/animate.min.css'],
-        'icons': ['vendor/font-awesome/css/fontawesome.min.css', 'vendor/simple-line-icons/css/simple-line-icons.css'],
+        'icons': ['vendor/components-font-awesome/css/fontawesome.min.css', 'vendor/simple-line-icons/css/simple-line-icons.css'],
         'screenfull': ['vendor/screenfull/dist/screenfull.js'],
-        'vector-map': ['vendor/ika.jvectormap/jquery-jvectormap-1.2.2.min.js',
-          '/ika.jvectormap/jquery-jvectormap-1.2.2.css'
-        ],
         'inputmask': ['vendor/jquery.inputmask/dist/jquery.inputmask.bundle.js'],
-        'codemirror': ['vendor/codemirror/lib/codemirror.js',
-          '/codemirror/lib/codemirror.css'
-        ],
-        // modes for common web files
-        'codemirror-modes-web': ['vendor/codemirror/mode/javascript/javascript.js',
-          '/codemirror/mode/xml/xml.js',
-          '/codemirror/mode/htmlmixed/htmlmixed.js',
-          '/codemirror/mode/css/css.js'
-        ],
         'taginput': [
           'vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput.css',
           '/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js'
